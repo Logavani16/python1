b729a2b9f1daa6fbc6b025179232f93e846f1d5c
Logavani16 <51950673+Logavani16@users.noreply.github.com>
2019-07-23 15:23:32 +0530

    Update 75.py

--- 75.py
+++ 75.py
@@ -1,8 +1,8 @@
-value=list(input())
-if len(value)%2==0:
-    value[int(len(value)/2)]='*'
-    value[int(len(value)/2)-1]='*'
+r1=list(input())
+if len(r1)%2==0:
+    r1[int(len(r1)/2)]='*'
+    r1[int(len(r1)/2)-1]='*'
 else:
-    value[int(len(value)/2)]='*'
-for i in range(len(value)):
-    print(value[i],end='')
+    r1[int(len(r1)/2)]='*'
+for i in range(len(r1)):
+    print(r1[i],end='')
